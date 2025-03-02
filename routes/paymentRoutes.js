2697731a85356d8f4de4d174cf34f8b3d31de5c5
yashnoor211 <122913343+yashnoor211@users.noreply.github.com>
2025-03-03 00:52:28 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { makePayment, getPayment, getPaymentStatus, updatePaymentStatus, getAllPayments } = require('../controllers/paymentController');
 
-router.post('/payments', makePayment); // Make a payment
-router.get('/payments/:id', getPayment); // Get payment details
-router.get('/payments/:id/status', getPaymentStatus); // Get payment status
-router.put('/payments/:id/status', updatePaymentStatus); // Update payment status
-router.get('/payments', getAllPayments); // Get all payments
+router.post('/payments', makePayment); 
+router.get('/payments/:id', getPayment); 
+router.get('/payments/:id/status', getPaymentStatus); 
+router.put('/payments/:id/status', updatePaymentStatus); 
+router.get('/payments', getAllPayments); 
 
 module.exports = router;
